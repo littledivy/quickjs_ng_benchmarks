2adcf9048c6b0c0706a07df5166423ab1be73398
Divy Srivastava <dj.srivastava23@gmail.com>
2023-11-18 10:05:09 +0530

    update stack size

--- run_sunspider.c
+++ run_sunspider.c
@@ -17,20 +17,18 @@ int execute(JSContext *ctx, const char *filename) {
 
     if (file == NULL) {
         fprintf(stderr, "Error opening file: %s\n", filename);
-        return 1; // Return an error code
+        return 1;
     }
 
-    // Get the file size
     fseek(file, 0, SEEK_END);
     long file_size = ftell(file);
     fseek(file, 0, SEEK_SET);
 
-    // Read the file content
     char *buffer = (char *)malloc(file_size + 1);
     if (buffer == NULL) {
         fclose(file);
         fprintf(stderr, "Memory allocation error\n");
-        return 1; // Return an error code
+        return 1;
     }
 
     size_t bytesRead = fread(buffer, 1, file_size, file);
@@ -40,18 +38,13 @@ int execute(JSContext *ctx, const char *filename) {
         fprintf(stderr, "Error reading file: %s\n", filename);
         printf("bytesRead: %zu, file_size: %ld\n", bytesRead, file_size);
         free(buffer);
-        return 1; // Return an error code
+        return 1;
     }
 
-    buffer[file_size] = '\0'; // Null-terminate the buffer
+    buffer[file_size] = '\0';
 
-    // Measure the start time
     clock_t start_time = clock();
-
-    // Execute the JavaScript code
     JSValue ret_val = JS_Eval(ctx, buffer, file_size, filename, JS_EVAL_TYPE_GLOBAL);
-
-    // Measure the end time
     clock_t end_time = clock();
     double execution_time = ((double)(end_time - start_time)) / CLOCKS_PER_SEC;
     total += execution_time;
@@ -61,7 +54,7 @@ int execute(JSContext *ctx, const char *filename) {
         JS_FreeValue(ctx, ret_val);
         free(buffer);
         free(fullpath);
-        return 1; // Return an error code
+        return 1;
     }
 
     printf("File: %s, Execution Time: %.4f seconds\n", filename, execution_time);
@@ -70,25 +63,25 @@ int execute(JSContext *ctx, const char *filename) {
     free(buffer);
     free(fullpath);
 
-    return 0; // Success
+    return 0;
 }
 
 int main() {
     JSRuntime *rt = JS_NewRuntime();
     JSContext *ctx = JS_NewContext(rt);
 
-    // Get LIST of tests
+    JS_SetMaxStackSize(rt, 864 * 1024); // 864 KB
+
     const char *filename = "sunspider-1.0/LIST";
     FILE *listFile = fopen(filename, "r");
 
     if (listFile == NULL) {
         fprintf(stderr, "Error opening file: %s\n", filename);
-        return 1; // Return an error code
+        return 1;
     }
 
     char buffer[100]; // Adjust the buffer size as needed
     while (fgets(buffer, sizeof(buffer), listFile) != NULL) {
-        // Remove newline character if present
         size_t len = strlen(buffer);
         if (len > 0 && buffer[len - 1] == '\n') {
             buffer[len - 1] = '\0';
@@ -101,10 +94,7 @@ int main() {
 
     printf("Total (ms): %.4f\n", total * 1000);
 
-    // Close the LIST file
     fclose(listFile);
-
-    // Cleanup QuickJS
     JS_FreeContext(ctx);
 
     return 0;
